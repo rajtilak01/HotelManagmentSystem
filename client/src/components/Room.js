8ecf564d881fbada037172ca2b7eebb2b8c0099d
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2023-12-29 11:34:34 +0530

    modal popup

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
+import {Modal, Button,Carousel} from "react-bootstrap";
 
 function Room({ room }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="row bs">
       <div className="col-md-4">
@@ -15,9 +21,34 @@ function Room({ room }) {
         </b>
 
         <div style={{float:'right'}}>
-            <button className="btn-primary btn ">View Details</button>
+            <button className="btn-primary btn" onClick={handleShow}>View Details</button>
         </div>
       </div>
+      
+
+      <Modal show={show} onHide={handleClose} size='lg'>
+        <Modal.Header>
+          <Modal.Title>{room.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Carousel prevLabel="" nextLabel="">
+      
+         {room.imageurls.map(url=>{
+          return <Carousel.Item>
+            <img className="d-block w-100 bigimg" src = {url}/>
+            </Carousel.Item>
+         })} 
+         
+       
+    </Carousel>
+    <p>{room.description}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
