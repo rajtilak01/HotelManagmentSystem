7d68a374ee678300d2c43a0b14b665a0c5e105b9
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2023-12-31 12:35:37 +0530

    single page hotel details frontend

--- client/src/screens/BookingScreen.js
+++ client/src/screens/BookingScreen.js
@@ -1,10 +1,10 @@
 import React, {useEffect, useState} from 'react'
-import { useParams } from 'react-router-dom';
+import { createRoutesFromElements, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 function BookingScreen() {
   const { roomid } = useParams();
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState();
   const [room, setRoom] = useState();
   
@@ -12,7 +12,7 @@ function BookingScreen() {
   useEffect(() => {
     const fetchDatabyId = async () => {
       try {
-        setLoading(true);
+        // setLoading(true);
         const data = (await axios.post("/api/rooms/getroombyid",{roomid})).data;
         setRoom(data); // Update state with response data
         setLoading(false);
@@ -27,11 +27,40 @@ function BookingScreen() {
   }, []);
    
   return (
-      <div>
-      <h1>Booking screen</h1>
-      <h1>Room id = {roomid}</h1>
+      <div className='m-5'>
+      {loading ? (<h1>loading.....</h1>) : error ?  (<h1>error....</h1>) : (<div>
+        <div className='row mt-5 justify-content-center bs'>
+          <div className='col-md-6'>
+            <h1>{room.name}</h1>
+            <img src={room.imageurls[0]} className='bigimg'/>
+          </div>
+          <div className='col-md-6'>
+            <div style={{textAlign:'right'}}>
+            <h1>Booking Detials</h1>
+            <br/>
+            <b>
+              <p>Name : </p>
+              <p>From Date : </p>
+              <p>To Date : </p>
+              <p>Max Count : {room.maxcount}</p>
+            </b>
+            </div>
+            <div style={{textAlign:'right'}}>
+              <b>
+              <h1>Amount</h1>
+              <br/>
+              <p>Total Days</p> 
+              <p>Rent per day : {room.rentperday}</p> 
+              <p>Total Ammount</p>  
+              </b>
+            </div>
+            <div style={{float:'right'}}>
+              <button className='btn btn-primary'>Pay Now</button>
+            </div>
+          </div>
+        </div>
+      </div>)}
     </div>
   );
 }
-
 export default BookingScreen
